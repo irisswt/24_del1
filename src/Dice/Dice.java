cece7edca41505499a87d594ea40394f5e72f65d
louismonty <louis.mail.dk@gmail.com>
2020-09-26 18:33:18 +0200

    privat variables made for Dice-class

--- src/Dice/Dice.java
+++ src/Dice/Dice.java
@@ -1,40 +1,38 @@
 package Dice;
 
 public class Dice {
+    private static int dice1;
+    private static int dice2;
     public static String toString(int x,int y)
     {
         return  x+" "+y;
     }
     public static String roll()
     {
-        int dice1 =  (int)(Math.random()*6)+1;
-        int dice2 =  (int)(Math.random()*6)+1;
+        dice1 =  (int)(Math.random()*6)+1;
+        dice2 =  (int)(Math.random()*6)+1;
 
         return toString(dice1,dice2);
     }
-    public static Boolean isDiceSame(String dice)
+    public static Boolean isDiceSame()
     {
-        char dice1 = dice.charAt(0);
-        char dice2 = dice.charAt(2);
-
         return dice1 == dice2;
     }
-    public static String setDice(int x, int y)
+    public static void setDice(int x, int y)
     {
-        return x+" "+y;
+        dice1 = x;
+        dice2 = y;
     }
-    public static int getValueDice(String dice)
+    public static int getValueDice()
     {
-        int dice1 = Character.getNumericValue(dice.charAt(0));
-        int dice2 = Character.getNumericValue(dice.charAt(2));
         return dice1+dice2;
     }
     public static void main(String[] args) {
         String Dice = roll();
-        Dice = setDice(1,5);
+        setDice(1,5);
         System.out.println(Dice);
-        System.out.println(isDiceSame(Dice));
-        System.out.println(getValueDice(Dice));
+        System.out.println(isDiceSame());
+        System.out.println(getValueDice());
 
 
 
