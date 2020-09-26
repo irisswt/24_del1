5a57b052ef986473c1bbf3cd351dd1da25b62ca1
louismonty <louis.mail.dk@gmail.com>
2020-09-26 17:01:49 +0200

    methods made public for Dice-class

--- src/Dice/Dice.java
+++ src/Dice/Dice.java
@@ -1,35 +1,43 @@
 package Dice;
 
 public class Dice {
-
     public static String toString(int x,int y)
     {
         return  x+" "+y;
     }
-    static String roll()
+    public static String roll()
     {
         int dice1 =  (int)(Math.random()*6)+1;
         int dice2 =  (int)(Math.random()*6)+1;
 
         return toString(dice1,dice2);
     }
-    static Boolean isDiceSame(String dice)
+    public static Boolean isDiceSame(String dice)
     {
         char dice1 = dice.charAt(0);
         char dice2 = dice.charAt(2);
 
         return dice1 == dice2;
     }
-    static String setDice(int x, int y)
+    public static String setDice(int x, int y)
     {
         return x+" "+y;
     }
-    static int getValueDice(String dice)
+    public static int getValueDice(String dice)
     {
         int dice1 = Character.getNumericValue(dice.charAt(0));
         int dice2 = Character.getNumericValue(dice.charAt(2));
         return dice1+dice2;
     }
+    public static void main(String[] args) {
+        String Dice = roll();
+        Dice = setDice(1,5);
+        System.out.println(Dice);
+        System.out.println(isDiceSame(Dice));
+        System.out.println(getValueDice(Dice));
 
+
+
+    }
 
 }
