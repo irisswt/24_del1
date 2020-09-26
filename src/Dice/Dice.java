66931ee6c8bf960d01c4935fe37406df448efdda
louismonty <louis.mail.dk@gmail.com>
2020-09-26 13:39:40 +0200

    first functions for dice for Dice-class

--- src/Dice/Dice.java
+++ src/Dice/Dice.java
@@ -1,4 +1,35 @@
 package Dice;
 
 public class Dice {
+
+    public static String toString(int x,int y)
+    {
+        return  x+" "+y;
+    }
+    static String roll()
+    {
+        int dice1 =  (int)(Math.random()*6)+1;
+        int dice2 =  (int)(Math.random()*6)+1;
+
+        return toString(dice1,dice2);
+    }
+    static Boolean isDiceSame(String dice)
+    {
+        char dice1 = dice.charAt(0);
+        char dice2 = dice.charAt(2);
+
+        return dice1 == dice2;
+    }
+    static String setDice(int x, int y)
+    {
+        return x+" "+y;
+    }
+    static int getValueDice(String dice)
+    {
+        int dice1 = Character.getNumericValue(dice.charAt(0));
+        int dice2 = Character.getNumericValue(dice.charAt(2));
+        return dice1+dice2;
+    }
+
+
 }
