d0b6c4eaaea06a5882410cc47436a9a06564e1d8
Terrell-byte <71126612+Terrell-byte@users.noreply.github.com>
2020-09-27 18:54:41 +0200

    everything works except 1 1 give you extra turn and when you win it print out player

--- src/com/company/Game/Game.java
+++ src/com/company/Game/Game.java
@@ -7,14 +7,16 @@ import com.company.Player.*;
 
 public class Game
 {
+    static int twoSix = 0;
     static String input;
     static boolean Game = true;
+    static int consecu = 0;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
         newGame();
 
-        while(Game)
+        while (Game)
         {
             gameLoop();
         }
@@ -23,11 +25,16 @@ public class Game
     public static void newGame()
     {
         System.out.println("Welcome dice roll. The goal of the game is to reach 40 points and then get to sixes. Player 1 start. Please type 'r' to roll");
+        Player.setScore(0);
+        Player.nextPlayer();
+        Player.setScore(0);
     }
 
     public static void endGame()
     {
-        System.out.print("Player " + Player.playerNumber() + " wins the game!");
+        System.out.println("Player " + Player.playerNumber() + " wins the game!");
+        newGame();
+
 
     }
 
@@ -36,13 +43,54 @@ public class Game
         Scanner scan = new Scanner(System.in);
         input = scan.nextLine();
 
+
+
         if(input.toLowerCase().equals("r"))
         {
-            System.out.println("Player" + Player.playerNumber());
             System.out.println(Dice.roll());
             Player.setScore(Dice.getValueDice() + Player.getScore());
-            System.out.println(Player.getScore());
-            Player.nextPlayer();
+
+            if(Dice.getValueDice() == 2)
+            {
+                Player.setScore(0);
+            }
+
+            if(Player.getScore() > 40)
+            {
+                Player.setScore(40);
+
+                if(Dice.isDiceSame())
+                {
+                    endGame();
+                }
+
+            }
+            if(Dice.getValueDice() == 12)
+            {
+                twoSix = twoSix + 1;
+            }
+            if(twoSix == 2)
+            {
+                endGame();
+            }
+            else
+            {
+            System.out.println("Player " + Player.playerNumber() + " Score: " + Player.getScore());
+            consecu = consecu + 1;
+
+            if(consecu >= 2)
+            {
+                twoSix = 0;
+            }
+
+            if(Dice.isDiceSame())
+            {
+                System.out.println("You got an extra turn!");
+            }
+            else
+            {
+                Player.nextPlayer();
+            }
         }
     }
 }
