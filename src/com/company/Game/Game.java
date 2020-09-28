7219f805c92888f3cbb3ea41095412aa224dfdbd
Terrell-byte <71126612+Terrell-byte@users.noreply.github.com>
2020-09-28 13:41:26 +0200

    - Created a method for Scanner so to avoid bugs with double typing - added a start new game option. - bug fixes - clean up code

--- src/com/company/Game/Game.java
+++ src/com/company/Game/Game.java
@@ -12,34 +12,52 @@ public class Game
     static boolean Game = true;
     static int consecu = 0;
 
+    public static void main(String[] args) { //Remove this when making Main script
+
+        newGame();
+
+        while(Game){
+            gameLoop();
+        }
+    }
+
+    public static void gameScanner()
+    {
+        Scanner scan = new Scanner(System.in);
+        input = scan.nextLine();
+    }
+
     public static void newGame()
     {
-        System.out.println("Welcome to dice roll. The goal of the game is to reach 40 points and then roll a pair. if the player rolls a pair before they reach 40 point, then they get an extra turn. if the player rolls a pair of 1, then they loose all their points Player 1 starts. Please type 'r' to roll");
+        System.out.println("Welcome to dice roll. The goal of the game is to reach 40 points and then roll a pair. if the player rolls a pair before they reach 40 point, then they get an extra turn. if the player rolls a pair of 1, then they loose all their points. Please type 'r' to roll");
         Player.setScore(0);
         Player.nextPlayer();
         Player.setScore(0);
         Player.nextPlayer();
         Dice.setDice(2,5);
+        consecu = 0;
+        twoSix = 0;
     }
 
     public static void endGame()
     {
         System.out.println("Player " + Player.playerNumber() + " wins the game!");
-        newGame();
+        Dice.setDice(2,5);
+        System.out.println("Type 'n' to start a new game!");
 
+        gameScanner();
 
+        if(input.toLowerCase().equals("n"))
+        {
+            newGame();
+        }
     }
 
     public static void gameLoop()
     {
-        Scanner scan = new Scanner(System.in);
-        input = scan.nextLine();
+        gameScanner();
 
 
-        if (Dice.getValueDice() == 2)
-        {
-            Player.setScore(0);
-        }
 
         if (input.toLowerCase().equals("r"))
         {
@@ -58,39 +76,39 @@ public class Game
                     endGame();
                 }
 
-            }
-            else
+            }else{
+
+                if (Dice.getValueDice() == 2)
                 {
-                    System.out.println("Player " + Player.playerNumber() + " Score: " + Player.getScore());
-                    consecu = consecu + 1;
+                    Player.setScore(0);
                 }
 
-            if (Dice.isDiceSame())
-            {
-                System.out.println("You got an extra turn!");
-            }
-            else
-                {
-                Player.nextPlayer();
-            }
+                System.out.println("Player " + Player.playerNumber() + " Score: " + Player.getScore());
+                consecu = consecu + 1;
 
+                if (Dice.isDiceSame())
+                {
+                    System.out.println("You got an extra turn!");
+
+                }else{
+                    Player.nextPlayer();
+                }
+            }
         }
 
         if (Dice.getValueDice() == 12)
         {
             twoSix = twoSix + 1;
         }
+
         if (twoSix == 2)
         {
             endGame();
         }
-        else
-            {
 
-            if (consecu >= 2) {
-                twoSix = 0;
-            }
-
+        if (consecu >= 2)
+        {
+            twoSix = 0;
         }
     }
 }
