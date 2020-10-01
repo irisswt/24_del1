41b4897005bdb5cf71a1ba1f3c92b51204fc87a5
Isak_s205452 <s205452@student.dtu.dk>
2020-10-01 23:32:34 +0200

    added test methods from Test-class and out commented them in Main-class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,19 @@
 package com.company;
 import com.company.Game.*;
-
+import Test.Test;
 
 
 public class Main { //method that executes newGame, rules and gameLoop from the Game class file.
 
     public static void main(String[] args) {
+        /*
+        Test.testRoll();
+        Test.testAllCombinations();
+        Test.testGameWinByScore();
+        Test.testGameWinByTwoSix();
+        Test.testTwoOnes();
+        */
+
         Game.newGame();
         while(true){
             Game.gameLoop();
