09797a1d538279598a45ff0ed6d266dc013dd3ad
louismonty <louis.mail.dk@gmail.com>
2020-10-01 13:40:19 +0200

    Mainfunction in Main-class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
+import com.company.Game.*;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-	
+        Game.newGame();
+        while(true){
+            Game.gameLoop();
+            Game.rules();
+        }
+
+
+
+
     }
 }
